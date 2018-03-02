03f0f83bf40fb77dcb35553db193361b2820f2eb
michelseb <sebastien.michel60@gmail.com>
2018-03-02 18:26:26 +0100

    Changement mécanique de saut + ghost

---
+++ Ghost.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Ghost : MonoBehaviour {
+
+    List<Vector2> oldPos;
+    List<Vector2> positions;
+    public GameObject ghost, instantiatedGhost;
+    //Ninja n;
+    float highest;
+    int step;
+    TextAsset ghostPos;
+    // Use this for initialization
+    void Start() {
+        //n = 
+        oldPos = new List<Vector2>();
+        positions = new List<Vector2>();
+        ghostPos = Resources.Load<TextAsset>(@"Data/ghost");
+        loadAll();
+        if (oldPos.Count > 0)
+        {
+            Debug.Log("Ghost");
+            instantiatedGhost = Instantiate(ghost, transform.position, Quaternion.identity);
+        }
+        step = 0;
+
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        positions.Add(transform.position);
+
+        if (instantiatedGhost != null)
+        {
+            if (oldPos.Count > step)
+            {
+                instantiatedGhost.transform.position = oldPos[step];
+                step++;
+            }
+            
+        }
+    }
+
+    public void saveAll(float highest)
+    {
+        if (highest > this.highest)
+        {
+            StreamWriter sw = new StreamWriter(@"Assets/Resources/Data/ghost.txt");
+            Debug.Log("Writing");
+            sw.WriteLine("" + highest);
+            foreach (Vector3 p in positions)
+            {
+                sw.WriteLine("" + p.x);
+                sw.WriteLine("" + p.y);
+            }
+            sw.Close();
+        }
+        
+    }
+
+    void loadAll()
+    {
+        if (ghostPos != null)
+        {
+            Debug.Log("Loading");
+            using (StreamReader sr = new StreamReader(new MemoryStream(ghostPos.bytes)))
+            {
+                highest = float.Parse(sr.ReadLine());
+                while (!sr.EndOfStream)
+                {
+                    float x = float.Parse(sr.ReadLine());
+                    float y = float.Parse(sr.ReadLine());
+                    oldPos.Add(new Vector2(x, y));
+                }
+            }
+        }
+    }
+
+}
---
+++ Ninja.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour, IDestructable {
+
+    public Deplacement d;
+    public Rigidbody2D r;
+    public Camera c;
+    public CameraBehaviour cam;
+    public TimeManager t;
+    Ghost g;
+    private float highestPoint;
+    bool selectMode;
+	// Use this for initialization
+    void Awake()
+    {
+        //SelectDeplacementMode();
+    }
+	void Start () {
+        d = FindObjectOfType<Deplacement>();
+        r = GetComponent<Rigidbody2D>();
+        c = FindObjectOfType<Camera>();
+        cam = FindObjectOfType<CameraBehaviour>();
+        t = FindObjectOfType<TimeManager>();
+        g = FindObjectOfType<Ghost>();
+        t.activated = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (d == null)
+        {
+            d = FindObjectOfType<Deplacement>();
+        }
+        
+        if (transform.position.y > highestPoint)
+        {
+            highestPoint = transform.position.y;
+        }
+    }
+
+    public void Die(Transform killer)
+    {
+        g.saveAll(highestPoint);
+        Destroy(d);
+        FixedJoint2D j = GetComponent<FixedJoint2D>();
+        if (j != null)
+        {
+            Destroy(j);
+        }
+        if (GetComponent<SpriteRenderer>() != null)
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        if (killer != null)
+        {
+            r.AddForce(killer.position - transform.position, ForceMode2D.Impulse);
+            r.AddTorque(20, ForceMode2D.Impulse);
+        }
+        StartCoroutine(Dying());
+    }
+
+    public IEnumerator Dying()
+    {
+        t.RestoreTime();
+        t.activated = false;
+        yield return new WaitForSeconds(2);
+        ScenesManager.BackToCheckpoint();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            if (d != null)
+                d.ReactToGround(collision);
+        }
+
+    }
+
+
+
+    private void SelectDeplacementMode()
+    {
+        selectMode = true;
+
+    }
+
+    void OnGUI()
+    {
+        if (selectMode)
+        {
+            if (GUI.Button(new Rect(0, Screen.height/2 - 20, Screen.width/2, 40), "Discrete mode"))
+            {
+                gameObject.AddComponent<DeplacementPrecis>();
+                selectMode = false;
+
+            }
+            else if(GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 - 20, Screen.width / 2, 40), "Furtive mode"))
+            {
+                gameObject.AddComponent<DeplacementFurtif>();
+                selectMode = false;
+            }  
+        }
+    }
+
+}
