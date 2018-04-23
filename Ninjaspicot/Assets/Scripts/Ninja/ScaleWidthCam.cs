4bde414deca0486f1847cffb77210a89ab0075ac
michelseb <sebastien.michel60@gmail.com>
2018-04-24 00:27:36 +0200

    Resolution multi-device

---
+++ Ninjaspicot/Assets/Scripts/Ninja/ScaleWidthCam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScaleWidthCam : MonoBehaviour {
+
+    public int targetWidth = 640;
+    public float pixelsToUnits = 100;
+
+	void Update () {
+
+        int height = Mathf.RoundToInt(targetWidth / (float)Screen.width * Screen.height);
+        GetComponent<Camera>().orthographicSize = height / pixelsToUnits / 2;
+
+	}
+}
