2977519c8b3faef37f298d6d5563779a0b131b42
michelseb <sebastien.michel60@gmail.com>
2018-04-27 18:49:50 +0200

    Ajout buildings + changement marche

---
+++ DeplacementFurtif.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementFurtif : Deplacement {
+
+    
+    Vector2 propulseVector;
+    bool readyToJump;
+    Collider2D col;
+    Trigger tri;
+    Coroutine walkOnWalls;
+    bool isWalking;
+    Touch to;
+
+    void Start () {
+        n = GetComponent<Ninja>();
+        SetMaxJumps(2);
+        GainAllJumps();
+        strength = 80;
+        canAttach = true;
+        tri = FindObjectOfType<Trigger>();
+        to = FindObjectOfType<Touch>();
+        OriginalRapidite = rapidite;
+    }
+
+    void Update()
+    {
+
+        //Debug.Log(n.contact.point.x + " " + n.contact.point.y);
+        if (Input.GetButtonDown("Fire1") && GetJumps() > 0)
+        {
+            
+            originClic = Input.mousePosition;
+            StartCoroutine(to.CreatePoints(originClic));
+            if (originClic.x < Screen.width / 2)
+            {
+                ninjaDir = Dir.Left;
+            }
+            else
+            {
+                ninjaDir = Dir.Right;
+            }
+            readyToJump = true;
+            t.Reset();
+
+        }
+
+        if (readyToJump && GetJumps() > 0)
+        {
+            propulseVector = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector3 originClicToWorld = c.ScreenToWorldPoint(originClic);
+            Vector3 propulseVectorToWorld = c.ScreenToWorldPoint(propulseVector);
+            if (isAttached && hinge != null)
+            {
+                
+                if ((propulseVectorToWorld - originClicToWorld).magnitude < 5)
+                {
+                    if (isWalking == false)
+                    {
+                        walkOnWalls = StartCoroutine(WalkOnWalls());
+                    }
+                    
+                    StartCoroutine(t.FadeAway());
+                    time.NormalTime();
+                }
+                if ((propulseVectorToWorld - originClicToWorld).magnitude > 5)
+                {
+                    //t.Reduce();
+                    t.DrawTraject(transform.position, GetComponent<Rigidbody2D>().velocity, Input.mousePosition, originClic, strength);
+                }
+
+            }
+            else
+            {
+                if ((propulseVectorToWorld - originClicToWorld).magnitude > 5)
+                {
+                    //t.Reduce();
+                    t.DrawTraject(transform.position, GetComponent<Rigidbody2D>().velocity, Input.mousePosition, originClic, strength);
+                }
+
+                if ((propulseVectorToWorld - originClicToWorld).magnitude < 5)
+                {
+                    StartCoroutine(t.FadeAway());
+                    time.NormalTime();
+                }
+            }
+        }
+        if (Input.GetButtonUp("Fire1") && GetJumps() > 0 && readyToJump)
+        {
+            to.Erase();
+            Vector3 originClicToWorld = c.ScreenToWorldPoint(originClic);
+            Vector3 propulseVectorToWorld = c.ScreenToWorldPoint(propulseVector);
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, t.line.GetPosition(2), LayerMask.GetMask("Default"));
+            Debug.DrawLine(transform.position, t.line.GetPosition(2), Color.green);
+            if (hit && hit.collider.tag != "ninja")
+            {
+                StartCoroutine(t.FadeAway());
+                StartCoroutine(time.RestoreTime());
+            }
+            else
+            {
+                if ((propulseVectorToWorld - originClicToWorld).magnitude > 5)
+                {
+                    if (isWalking == true)
+                    {
+                        StopCoroutine(walkOnWalls);
+                        isWalking = false;
+                    }
+
+                    Jump(Input.mousePosition, strength);
+                }
+            }
+            jumped = true;
+            readyToJump = false;
+        }
+
+
+    }
+
+
+    public IEnumerator WalkOnWalls()
+    {
+        
+        isWalking = true;
+         
+        motor = hinge.motor;
+        while (Input.GetButton("Fire1"))
+        {
+            col = n.contact.collider;
+
+            if (ninjaDir == Dir.Right)
+            {
+                motor.motorSpeed = rapidite;
+            }
+            else if (ninjaDir == Dir.Left)
+            {
+                motor.motorSpeed = -rapidite;
+            }
+
+            hinge.motor = motor; 
+            hinge.anchor = transform.InverseTransformPoint(n.contact.point);
+            hinge.connectedAnchor = transform.InverseTransformPoint(n.contact.point);
+
+            yield return null;
+        }
+        motor.motorSpeed = 0;
+        hinge.motor = motor;
+        hinge.anchor = transform.InverseTransformPoint(n.contact.point);
+        hinge.connectedAnchor = transform.InverseTransformPoint(n.contact.point);
+        isWalking = false;
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(n.contact.point, .5f);
+    }
+
+
+}
---
+++ Ninja.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour, IDestructable {
+
+    public ContactPoint2D contact, previousContact;
+    public Deplacement d;
+    public Rigidbody2D r;
+    public Camera c;
+    public CameraBehaviour cam;
+    public TimeManager t;
+    Ghost g;
+    private float highestPoint;
+    bool selectMode;
+	// Use this for initialization
+    void Awake()
+    {
+        //SelectDeplacementMode();
+    }
+	void Start () {
+        d = FindObjectOfType<Deplacement>();
+        r = GetComponent<Rigidbody2D>();
+        c = FindObjectOfType<Camera>();
+        cam = FindObjectOfType<CameraBehaviour>();
+        t = FindObjectOfType<TimeManager>();
+        g = FindObjectOfType<Ghost>();
+        t.activated = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (d == null)
+        {
+            d = FindObjectOfType<Deplacement>();
+        }
+        
+        
+
+        if (transform.position.y > highestPoint)
+        {
+            highestPoint = transform.position.y;
+        }
+    }
+
+    public void Die(Transform killer)
+    {
+        g.saveAll(highestPoint);
+        Destroy(d);
+        HingeJoint2D j = GetComponent<HingeJoint2D>();
+        if (j != null)
+        {
+            Destroy(j);
+        }
+        if (GetComponent<SpriteRenderer>() != null)
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        if (killer != null)
+        {
+            if (killer.GetComponent<SpriteRenderer>() != null)
+            {
+                killer.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+            r.AddForce(killer.position - transform.position, ForceMode2D.Impulse);
+            r.AddTorque(20, ForceMode2D.Impulse);
+        }
+        StartCoroutine(Dying());
+    }
+
+    public IEnumerator Dying()
+    {
+        t.NormalTime();
+        t.activated = false;
+        yield return new WaitForSeconds(2);
+        ScenesManager.BackToCheckpoint();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("entrée");
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            if (d != null)
+            {
+                d.ReactToGround(collision);
+
+                d.hinge.anchor = transform.InverseTransformPoint(contact.point);
+                if (d.ninjaDir == Deplacement.Dir.Right)
+                {
+                    contact = collision.contacts[collision.contacts.Length - 1];
+                }
+                else
+                {
+                    contact = collision.contacts[0];
+                }
+                previousContact = contact;
+                d.hinge.anchor = transform.InverseTransformPoint(contact.point);
+                d.hinge.connectedAnchor = transform.InverseTransformPoint(contact.point);
+            }
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (d != null)
+        {
+            if (d.hinge != null)
+            {
+                d.hinge.anchor = transform.InverseTransformPoint(contact.point);
+                
+                {
+                    contact = SelectContactPoint(collision.contacts, previousContact);//collision.contacts[collision.contacts.Length - 1];
+                }
+                else
+                {
+                    contact = SelectContactPoint(collision.contacts, previousContact); //collision.contacts[0];
+                }
+                d.hinge.anchor = transform.InverseTransformPoint(contact.point);
+                d.hinge.connectedAnchor = transform.InverseTransformPoint(contact.point);
+            }
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Sortie");
+    }
+
+
+
+
+    private void SelectDeplacementMode()
+    {
+        selectMode = true;
+
+    }
+
+    void OnGUI()
+    {
+        if (selectMode)
+        {
+            if (GUI.Button(new Rect(0, Screen.height/2 - 20, Screen.width/2, 40), "Discrete mode"))
+            {
+                gameObject.AddComponent<DeplacementPrecis>();
+                selectMode = false;
+
+            }
+            else if(GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 - 20, Screen.width / 2, 40), "Furtive mode"))
+            {
+                gameObject.AddComponent<DeplacementFurtif>();
+                selectMode = false;
+            }  
+        }
+    }
+
+
+    ContactPoint2D SelectContactPoint(ContactPoint2D[] contacts, ContactPoint2D previous)
+    {
+        if (contacts.Length == 0)
+        {
+            if (d.ninjaDir == Deplacement.Dir.Right)
+            {
+
+            }
+        }
+        ContactPoint2D cont;
+        foreach (ContactPoint2D c in contacts)
+        {
+
+        }
+
+        return cont;
+    }
+}
