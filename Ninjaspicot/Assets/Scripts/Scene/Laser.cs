1cf09e3363556f177736b949ac53f42b7367e55b
Sébastien Michel <sebastien.michel@syfadis.fr>
2021-08-22 01:28:36 +0200

    Lasers

--- Ninjaspicot/Assets/Scripts/Scene/Laser.cs
+++ Ninjaspicot/Assets/Scripts/Scene/Laser.cs
@@ -1,6 +1,37 @@
 ﻿using UnityEngine;
 
-public class Laser : MonoBehaviour {
+public class Laser : MonoBehaviour
+{
+
+    [SerializeField] private RectTransform _start;
+    [SerializeField] private RectTransform _end;
+    [SerializeField] private int _pointsAmount;
+
+    private LineRenderer _laser;
+    private PolygonCollider2D _collider;
+
+    private void Awake()
+    {
+        _laser = GetComponent<LineRenderer>();
+        _collider = GetComponent<PolygonCollider2D>();
+    }
+
+    private void Start()
+    {
+        _laser.positionCount = _pointsAmount;
+        _laser.SetPosition(0, _start.position);
+        _laser.SetPosition(_pointsAmount - 1, _end.position);
+        SetCollider();
+    }
+
+    private void Update()
+    {
+        for (int i = 1; i < _pointsAmount - 1; i++)
+        {
+            var pos = _start.position + ((_end.position - _start.position) * (i + 1) / _pointsAmount);
+            _laser.SetPosition(i, new Vector2(pos.x, pos.y) + new Vector2(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f)));
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -8,5 +39,20 @@ public class Laser : MonoBehaviour {
         {
             Hero.Instance.Die();
         }
+    }
+
+    private void SetCollider()
+    {
+        var startCorners = new Vector3[4];
+        var endCorners = new Vector3[4];
+        _start.GetWorldCorners(startCorners);
+        _end.GetWorldCorners(endCorners);
+        _collider.SetPath(0, new Vector2[]
+        {
+            transform.InverseTransformPoint(startCorners[0]),
+            transform.InverseTransformPoint(startCorners[3]),
+            transform.InverseTransformPoint(endCorners[0]),
+            transform.InverseTransformPoint(endCorners[3])
+        });
     }
 }
