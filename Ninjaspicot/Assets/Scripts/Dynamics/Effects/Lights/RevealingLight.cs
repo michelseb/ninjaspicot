8a5a5c7162499f2333ab35a4b1113d7b0d75b475
michelseb <sebastien.michel60@gmail.com>
2022-09-02 14:45:30 +0200

    Revealing lights blocked by walls

--- Ninjaspicot/Assets/Scripts/Dynamics/Effects/Lights/RevealingLight.cs
+++ Ninjaspicot/Assets/Scripts/Dynamics/Effects/Lights/RevealingLight.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using ZepLink.RiceNinja.Dynamics.Interfaces;
+using ZepLink.RiceNinja.Utils;
 
 namespace ZepLink.RiceNinja.Dynamics.Effects.Lights
 {
@@ -15,46 +16,54 @@ namespace ZepLink.RiceNinja.Dynamics.Effects.Lights
 
         private void OnTriggerStay2D(Collider2D collision)
         {
-            if (!TryGetSeeable(collision, out ISeeable seeable, true))
+            var id = collision.GetInstanceID();
+
+            if (!TryGetSeeable(id, collision, out ISeeable seeable))
                 return;
 
+            if (CastUtils.LineCast(Transform.position, collision.transform.position, layerMask: CastUtils.OBSTACLES))
+            {
+                if (_seeables.ContainsKey(id))
+                {
+                    seeable.Hide();
+                    _seeables.Remove(id);
+                }
+
+                return;
+            }
+
+            if (_seeables.ContainsKey(id))
+                return;
+
+            _seeables.Add(id, seeable);
             seeable.Reveal();
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
-            if (!TryGetSeeable(collision, out ISeeable seeable, false))
+            var id = collision.GetInstanceID();
+
+            if (!TryGetSeeable(id, collision, out ISeeable seeable))
                 return;
 
             seeable.Hide();
+            _seeables.Remove(id);
         }
 
-        private bool TryGetSeeable(Collider2D collision, out ISeeable seeable, bool add)
+        private bool TryGetSeeable(int id, Collider2D collision, out ISeeable seeable)
         {
             seeable = default;
 
             if (!collision.CompareTag("hero"))
                 return false;
 
-            var id = collision.GetInstanceID();
-
             if (_seeables.ContainsKey(id))
             {
-                if (add) return false;
-
                 seeable = _seeables[id];
-            }
-            else
-            {
-                if (!collision.TryGetComponent(out seeable))
-                    return false;
-
-                if (add) _seeables.Add(id, seeable);
+                return true;
             }
 
-            if (!add) _seeables.Remove(id);
-
-            return true;
+            return collision.TryGetComponent(out seeable);
         }
     }
 }
